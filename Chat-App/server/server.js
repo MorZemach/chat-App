9d2009a0ff88ac656a8caec07f80ae445433d70b
Mor Zemach <90982428+MorZemach@users.noreply.github.com>
2022-03-18 19:11:21 +0200

    added socket connection

--- Chat-App/server/server.js
+++ Chat-App/server/server.js
@@ -1,10 +1,26 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
+
 const publicPath = path.join(__dirname, "/../public");
 const port = process.env.PORT || 3000
-
 let app = express();
+let server = http.createServer(app);
+let io = socketio(server);
+
 app.use(express.static(publicPath));
-app.listen(port, () => {
+
+// backend listening to the connection that the frontend ask from him (in index.html file)
+// when user will be looking for "localhost:3000" he will be connected (the front will send the back the message and the back will listening)
+io.on('connection', (socket) => {
+    console.log("A new user just connected");
+
+    socket.on('disconnect', () => {
+        console.log("User was disconnected from server.");
+    });
+});
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 })
