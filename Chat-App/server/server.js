0a89c5dd822cad8c8475cf0440a44906e3d56452
Mor Zemach <90982428+MorZemach@users.noreply.github.com>
2022-03-19 17:27:43 +0200

    adding geological location to the chat

--- Chat-App/server/server.js
+++ Chat-App/server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const { generateMessage, generateLocationMessage } = require('./utils/message');
 
 const publicPath = path.join(__dirname, "/../public");
 const port = process.env.PORT || 3000
@@ -15,29 +16,27 @@ app.use(express.static(publicPath));
 // when user will be looking for "localhost:3000" he will be connected (the front will send the back the message and the back will listening)
 io.on('connection', (socket) => {
     console.log("A new user just connected");
+    // When a user is connect the app I can know his socket id using "socket.id" like the console. 
+    // It's the same socket id the client side got.
+    // We should save it to the database
+    console.log("Your socket id is: " + socket.id);
 
     //----------------------- socket is for only one user || io is for all users at the same time ---------------------------//
     // Server make a new message and send it to the client
     // every users that connect the sever ('localhost:3000') will be get this message when he'll connect.
-    socket.emit('newMessage', {
-        from: "Admin",
-        text: "Welcome to the chat app!",
-        createdAt: new Date().getTime()
-    })
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app!'));
 
-    socket.broadcast.emit('newMessage', {
-        from: "Admin",
-        text: "New user joined!",
-        createdAt: new Date().getTime()
-    })
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined!'));
 
-    socket.on('createMessage', (message) => {
+    socket.on('createMessage', (message, callback) => {
         console.log("createMessage", message);
-        io.emit('newMessage', {
-            from: message.from, 
-            text: message.text,
-            createdAt: new Date().getTime()
-        })
+        io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('This is the server!');
+    })
+
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin',
+        coords.lat, coords.lng))
     })
 
     socket.on('disconnect', () => {
